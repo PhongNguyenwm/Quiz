4815b570dbfd643e0d557e7ad9a294c6860cfda6
VSOP40 <phongnguyenwm3@gmail.com>
2024-11-20 13:15:29 +0700

    video #17 State

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,8 +1,18 @@
 import React from "react";
 class MyComponent extends React.Component {
+  state = {
+    name: "Phong",
+    address: "HCM",
+    age: 35,
+  };
+
   // JSX
   render() {
-    return <div>My first Component {Math.random()}</div>;
+    return (
+      <div>
+        My name is {this.state.name} and I'm form {this.state.address}
+      </div>
+    );
   }
 }
 

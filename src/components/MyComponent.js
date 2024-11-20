bcacaea6344e5503b71398ed86f9004018ab9b31
VSOP40 <phongnguyenwm3@gmail.com>
2024-11-20 14:34:00 +0700

    video #20 setStage - updateState

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,11 +6,29 @@ class MyComponent extends React.Component {
     age: 35,
   };
 
+  handleClick = (e) => {
+    console.log("Click my button");
+    this.setState({ name: "Yên" });
+    this.setState({ age: Math.floor(Math.random() * 100 + 1) });
+  };
+
+  handleOnMouseOver = (e) => {
+    console.log(e.pageX);
+  };
+
   // JSX
   render() {
     return (
       <div>
         My name is {this.state.name} and I'm form {this.state.address}
+        <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
+        <button
+          onClick={(e) => {
+            this.handleClick(e);
+          }}
+        >
+          Click me
+        </button>
       </div>
     );
   }

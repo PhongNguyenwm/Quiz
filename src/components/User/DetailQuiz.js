ab34abf8ea4f0e7eed378cd18f57afead566df1a
VSOP40 <phongnguyenwm3@gmail.com>
2024-11-26 15:05:23 +0700

    video #83 process data form BE - use lodash to gruop questionId => use forEach to return result as wish

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
+import _ from "lodash";
 
 const DetailQuiz = (props) => {
   const params = useParams();
@@ -10,6 +11,26 @@ const DetailQuiz = (props) => {
     const fetchQuestions = async () => {
       const res = await getDataQuiz(quizId);
       console.log("check data res:", res);
+      if (res && res.EC === 0) {
+        let raw = res.DT;
+        let data = _.chain(raw)
+          .groupBy("id")
+          .map((value, key) => {
+            let answers = [];
+            let questionDesc,
+              img = null;
+            value.forEach((item, index) => {
+              if (index === 0) {
+                questionDesc = item.description;
+                img = item.image;
+              }
+              answers.push(item.answers);
+            });
+            return { questionId: key, answers, questionDesc, img };
+          })
+          .value();
+        console.log(data);
+      }
     };
     fetchQuestions();
   }, [quizId]);
